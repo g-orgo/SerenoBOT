f3b9924e5180da7f099a0169fc3a418d5d86b5a2
unknown <gabrielorgo5@gmail.com>
2021-03-20 02:22:52 -0300

    credentials fix

--- credentials.js
+++
@@ -1,9 +0,0 @@
-let discordCredentials = {
-    clientID: 822373586790383616,
-    clientSecret: "rooG8h9lojYCMim7BjWZAjQ1rDZWLzHI",
-    publicKey: "e5a023f534498a9f203f7e4ee242f2128881eca60222d46dfe216e4c160fd0f3",
-    email: "videodumbster323@gmail.com",
-    password: "serenobot123"
-};
-
-module.exports = discordCredentials
