694edbb2888a5b8759af52a4c198b72b1c4d9a18
unknown <gabrielorgo5@gmail.com>
2021-03-22 04:51:58 -0300

    Setting discord js

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
-const discordBOT = require('discord.js');
-const discordCredentials = require ('./credentials')
+const discordAPI = require('discord.js');
+const expressApp = require ('./server')
+const credentials = require('./credentials')
 
-const bot = new discordBOT.Client()
+const bot = new discordAPI.Client()
+const port = credentials.port
+const app = expressApp
+const discordCredentials = credentials.discordCredentials
+const prefix = '!'
 
+// INIT
 bot.login(discordCredentials.botToken)
+.then()
+.catch((err)=>{console.log(err)})
 
 bot.on('ready', ()=>{
-    console.log(`Sereno logged in!\n${discordCredentials.oAuth2URL}\n`)
+    console.log(`[SERENO]: Beep boop!`)
+})
+
+
+// MESSAGE ENG
+bot.on('message', message =>{
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).trim().split(' ');
+    const command = args.shift().toLowerCase();
+
+    if(command === 'ping'){
+        message.channel.send(`${message.id}`);
+        /* message.delete(`${message.id}`) */
+    }else if(command === 'beep'){
+        message.channel.send('boop garaio');
+    };
 })
--- server.js
+++ server.js
@@ -1,13 +1,9 @@
 const express = require('express')
-const credentials = require('./credentials')
 
 const app = express()
-const port = credentials.port
 
-app.listen(port, () => {
-    console.log(`Sereno is on port ${port}`)
+app.get('/callback', (req, res)=>{
+    res.send('worked').status(200)
 })
 
-app.get('/', (req, res)=>{
-    res.send('')
-})
+module.exports = app
