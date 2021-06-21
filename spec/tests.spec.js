2d4e2507b30bd278ded67c535beaba601759e21f
Gabriel <gabrielorgo5@gmail.com>
2021-06-21 13:21:29 -0300

    Changing testing framework

--- spec/tests.spec.js
+++ spec/tests.spec.js
@@ -2,13 +2,14 @@ function description(msg){
     return `| ${msg}`
 }
 describe("Trivial logics", function () {
-    it(description("Return random color system"), function () {
+    test(description("Return a random value in range"), function () {
         var colors = ["#f2499d", "#8ee5f5", "#64f59e"];
         expect(colors).toContain(
             colors[Math.floor(Math.random() * colors.length)]
         );
     });
-    it(description("Winrate system"), function () {
+
+    test(description("Winrate system"), function () {
         let ataq = 150;
         let bloq = 80;
         expect((ataq + bloq / 100).toFixed(2)).toBe("150.80");
